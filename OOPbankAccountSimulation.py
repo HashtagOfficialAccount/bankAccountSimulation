21cab7a6c80f15e5da550b6f7dee8442fea583b8
Srinikash Venki <75814786+HashtagOfficialAccount@users.noreply.github.com>
2022-04-21 08:24:21 -0700

    Update OOPbankAccountSimulation.py

--- OOPbankAccountSimulation.py
+++ OOPbankAccountSimulation.py
@@ -39,19 +39,39 @@ while True:
         loop += 1
 
     print("You have 3 options")
+    print("n = create new account")
     print("d = deposit")
     print("w = withdraw")
     print("e = exit simulation ")
+    
     choice = input("What would you like to do? Enter an option: ")
+    if choice == "n" or choice == "create new account":
+        print("Let's create you a new account, please enter your details here:")
+        newacc_name = input("Enter your name: ")
+        newacc_password = input("Enter your password: ")
+        newacc_balance = int(input("Enter your initial deposit: "))
+        print("Creating your account...")
+        time.sleep(3)
+        name = bankAccount(newacc_name,newacc_password,newacc_balance)
+        print("Your account is all setup!")
+        loop += 1
+     
     if choice == "d" or choice == "deposit":
-        deposit_amount = int(input("How much would you like to deposit: "))
-        name.deposit(deposit_amount)
+        if loop >= 2:
+            print("Which account would you like to use?")
+            acc_choice = input("Please enter the name you used to register: ")
+            deposit_amount = int(input("How much would you like to deposit: "))
+            acc_choice.deposit(deposit_amount)
+        else:
+            deposit_amount = int(input("How much would you like to deposit: "))
+            name.deposit(deposit_amount)
+        
     if choice == "w" or choice == "withdraw":
+        if loop >= 2:
+            print("Which account would you like to use?")
+            acc_choice = input("Please enter the name you used to register: ")
         withdraw_amount = int(input("How much would you like to withdraw: "))
         name.withdraw(withdraw_amount)
     if choice == "e" or choice == "exit":
         print("Thank you for trying out the simulation.")
         exit()
-        
-        
-
